4a0dc80af0e0a16d5f72fbb6a1147f2619887e26
Arnaud LAYEC <arnaudlayec@gmail.com>
2026-03-16 17:36:50 +0100

    precommit

--- oca_website/__manifest__.py
+++ oca_website/__manifest__.py
@@ -1,34 +1,32 @@
-
 {
     "name": "OCA Website Theme",
     "version": "18.0.1.0.0",
     "category": "Theme",
     "summary": "OCA Website Theme",
-    "description": "",
-    "author": "Akretion",
+    "author": "Akretion, Odoo Community Association (OCA)",
     "company": "Akretion",
     "maintainer": "Akretion",
-    "website": "https://www.akretion.com",
-    'depends': [
-        'website',
+    "website": "https://github.com/OCA/oca-custom",
+    "depends": [
+        "website",
     ],
     "data": [
-        'data/images.xml',
-    ],       
-    'images': [
-        'static/description/favicon.png',
-        'static/description/theme_screenshot.png'
+        "data/images.xml",
     ],
-    'assets': {
-        'web._assets_primary_variables': [
-            'oca_website/static/src/scss/primary_variables.scss',
-        ],  
-         'web.assets_frontend': [
-              'oca_website/static/src/scss/layout/website_theme.scss',
-         ],
+    "images": [
+        "static/description/favicon.png",
+        "static/description/theme_screenshot.png",
+    ],
+    "assets": {
+        "web._assets_primary_variables": [
+            "oca_website/static/src/scss/primary_variables.scss",
+        ],
+        "web.assets_frontend": [
+            "oca_website/static/src/scss/layout/website_theme.scss",
+        ],
     },
-    'license': 'LGPL-3',
-    'installable': True,
-    'auto_install': False,
-    'application': False,
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
