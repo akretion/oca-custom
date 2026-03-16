3953e8a7ba7107395737072cab9a1042769b6ec2
Dora Jurcevic <dora.jurcevic@akretion.com>
2026-03-16 14:46:28 +0100

    ADD: website theme module add

---
+++ oca_website/__manifest__.py
@@ -0,0 +1,33 @@
+
+{
+    "name": "OCA Website Theme",
+    "version": "18.0.1.0.0",
+    "category": "Theme/eCommerce",
+    "summary": "OCA Website Theme",
+    "description": "",
+    "author": "Akretion",
+    "company": "Akretion",
+    "maintainer": "Akretion",
+    "website": "https://www.akretion.com",
+    'depends': [
+        'website',
+    ],
+    "data": [
+        'data/images.xml',       
+    ],
+    'images': [
+        'oca_website/static/description/theme_screenshot.png'
+    ],
+    'assets': {
+        'web._assets_primary_variables': [
+            'oca_website/static/src/scss/primary_variables.scss',
+        ],
+         'web.assets_frontend': [
+              'oca_website/static/src/scss/layout/website_theme.scss',
+         ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
