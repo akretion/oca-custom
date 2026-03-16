6ddcd31432487814ee9e454b582db02848fc3fda
Arnaud LAYEC <arnaudlayec@gmail.com>
2026-03-16 10:33:32 +0100

    fixup! [MOVE][18.0] Move sponsor management to new 'oca_sponsor' + and free the dependency with 'website_crm_partner_assign'
    Fix migration script

--- website_oca_integrator/migrations/18.0.1.0.2/pre-migrate.py
+++ website_oca_integrator/migrations/18.0.1.0.2/pre-migrate.py
@@ -7,7 +7,7 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     view = env.ref(
         "website_oca_integrator.view_crm_partner_geo_form_inherit",
-        raise_if_not_found=False,
+        raise_if_not_found=True,
     )
     if view:
         view.unlink()
